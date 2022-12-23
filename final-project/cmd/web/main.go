4ce23bc9bc6960ae9fde6d265d85b68fc89d9477
calvarado2004 <carlos-alvarado@outlook.com>
2022-12-23 12:26:15 -0500

    final project

---
+++ final-project/cmd/web/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+func main() {
+	// connect to the database
+	db := initDB()
+
+	// create web sessions
+	session := initSession()
+
+	// create loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// create channels
+
+	// create wait group
+	wg := sync.WaitGroup{}
+
+	// set up application config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		Wait:     &wg,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+	}
+
+	// set up email
+
+	// listen for web connections
+	app.serve()
+
+}
+
+func (app *Config) serve() {
+	// serve the web application
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	app.InfoLog.Printf("Starting server on port %s", webPort)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		app.ErrorLog.Fatal(err)
+	}
+
+}
+
+func initDB() *sql.DB {
+	// connect to the database
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Could not connect to the database")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	// connect to the database
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Printf("Could not connect to the database: %s", err)
+		} else {
+			log.Printf("Connected to the database")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Printf("Backing off and trying again")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+
+	}
+
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// open the database connection
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+
+	return redisPool
+}
---
+++ final-project/cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// set up middleware
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", app.HomePage)
+
+	return mux
+}
